9cd258986ce04f60fcf62ad07c127d893984baf7
jorgerioja <riojaj06@gmail.com>
2024-08-05 16:54:46 -0500

    home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  currentItem = 0;
+  items!: HTMLElement[];
 
+  ngOnInit() {
+    this.items = Array.from(document.querySelectorAll('.carousel-item'));
+    this.showItem(this.currentItem);
+  }
+
+  showItem(index: number) {
+    this.items.forEach((item, i) => {
+      item.classList.toggle('active', i === index);
+    });
+  }
+
+  prevSlide() {
+    this.currentItem = (this.currentItem - 1 + this.items.length) % this.items.length;
+    this.showItem(this.currentItem);
+  }
+
+  nextSlide() {
+    this.currentItem = (this.currentItem + 1) % this.items.length;
+    this.showItem(this.currentItem);
+  }
+
+  goToSlide(index: number) {
+    this.currentItem = index;
+    this.showItem(this.currentItem);
+  }
 }
